9fdf9ae64a263f8b6e7e21be75121d6148134f92
Mohammed Gias Uddin Al Mamun <93403454+mgamamunbd@users.noreply.github.com>
2022-06-06 19:34:18 +0400

    address

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ function Home() {
     <div className='home container'>
       <h1>Home</h1>
       <p>Site is under construction</p>
+      <p>Contact: MGA Mamun</p>
+      <p>GSM: +971 55 655 84 32</p>
+      <p>GSM: +971 50 32 99 101</p>
+      <p>Email: mgamamunbd@gmail.com</p>
     </div>
   );
 }
