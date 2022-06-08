cef6d9cae279d1a127fc5a32f5146ac9a6c613ae
Mohammed Gias Uddin Al Mamun <93403454+mgamamunbd@users.noreply.github.com>
2022-06-08 23:36:32 +0400

    tailwindcss added

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import React from "react";
 const index = () => {
   return (
     <div>
+      <p className='text-3xl font-bold'>Site is under construction</p>
       <h1>Contact</h1>
-      <p>Site is under construction</p>
       <p>Contact: MGA Mamun</p>
       <p>GSM: +971 55 655 84 32</p>
       <p>GSM: +971 50 32 99 101</p>
